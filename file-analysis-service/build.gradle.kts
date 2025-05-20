541ffdc1eebd0904b572dee2c6cd70660006744f
ProgrammerPeasant <poland-fish@yandex.ru>
2025-05-20 21:38:34 +0300

    build and eureka

---
+++ file-analysis-service/build.gradle.kts
@@ -0,0 +1,8 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("org.postgresql:postgresql")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+}
