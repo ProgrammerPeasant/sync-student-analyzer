f43d9fbcedb0ed83d9b6df929d5f9f64da41990d
ProgrammerPeasant <poland-fish@yandex.ru>
2025-05-22 16:14:44 +0300

    fast gateway fix

--- api-gateway/src/main/java/com/textscan/gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/textscan/gateway/config/GatewayConfig.java
@@ -1,26 +1,3 @@
-//package com.textscan.gateway.config;
-//
-//import org.springframework.cloud.gateway.route.RouteLocator;
-//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class GatewayConfig {
-//
-//    @Bean
-//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route("file-storage-service", r -> r
-//                        .path("/api/files/**", "/swagger-ui/**", "/v3/api-docs/**")
-//                        .uri("lb://file-storage-service"))
-//                .route("file-analysis-service", r -> r
-//                        .path("/api/analysis/**", "/swagger-ui/**", "/v3/api-docs/**")
-//                        .uri("lb://file-analysis-service"))
-//                .build();
-//    }
-//}
-
 package com.textscan.gateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -34,24 +11,20 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                // Маршруты для API file-storage-service
                 .route("file-storage-service-api", r -> r
                         .path("/api/files/**")
                         .uri("lb://file-storage-service"))
-                // Маршрут для документации OpenAPI file-storage-service
                 .route("file-storage-service-openapi", r -> r
-                        .path("/v3/api-docs/file-storage-service") // Уникальный путь на шлюзе
-                        .filters(f -> f.rewritePath("/v3/api-docs/file-storage-service", "/v3/api-docs")) // Удаляем префикс группы перед отправкой сервису
+                        .path("/v3/api-docs/file-storage-service")
+                        .filters(f -> f.rewritePath("/v3/api-docs/file-storage-service", "/v3/api-docs"))
                         .uri("lb://file-storage-service"))
 
-                // Маршруты для API file-analysis-service
                 .route("file-analysis-service-api", r -> r
                         .path("/api/analysis/**")
                         .uri("lb://file-analysis-service"))
-                // Маршрут для документации OpenAPI file-analysis-service
                 .route("file-analysis-service-openapi", r -> r
-                        .path("/v3/api-docs/file-analysis-service") // Уникальный путь на шлюзе
-                        .filters(f -> f.rewritePath("/v3/api-docs/file-analysis-service", "/v3/api-docs")) // Удаляем префикс группы перед отправкой сервису
+                        .path("/v3/api-docs/file-analysis-service")
+                        .filters(f -> f.rewritePath("/v3/api-docs/file-analysis-service", "/v3/api-docs"))
                         .uri("lb://file-analysis-service"))
                 .build();
     }
